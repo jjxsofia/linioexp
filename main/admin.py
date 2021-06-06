bdb711436419b9c0d55775663ffcebd68ef915c9
Sofía <sn.jaraj@gmail.com>
2021-06-05 22:36:59 -0500

    mi primer commit

---
+++ main/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+
+from .models import *
+
+class ClienteInline(admin.TabularInline):
+    model=Cliente
+
+class ColaboradorInline(admin.TabularInline):
+    model=Colaborador
+
+class ProfileAdmin(admin.ModelAdmin):
+    inlines = [
+        ClienteInline,
+        ColaboradorInline
+    ]
+
+class ProductoImageInline(admin.TabularInline):
+    model=ProductoImage
+
+class ProductoAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductoImageInline,
+    ]
+
+# Register your models here.
+admin.site.register(Localizacion)
+#admin.site.register(Producto)
+admin.site.register(Categoria)
+admin.site.register(Proveedor)
+
+admin.site.register(Cliente)
+admin.site.register(Colaborador)
+admin.site.register(Profile, ProfileAdmin)
+
+#Lab 6 
+admin.site.register(Producto, ProductoAdmin)
+
